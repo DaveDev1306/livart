188cd434b4d77be577434b9eea71cab9400095ff
David Magueijo <davedev1984@gmail.com>
2020-09-29 12:17:11 +0100

    Added resources and few controller for art_work changes'

--- app/controllers/art_works_controller.rb
+++ app/controllers/art_works_controller.rb
@@ -1,4 +1,5 @@
 class ArtWorksController < ApplicationController
+  before_action :authenticate_user!
 
   def index
     @art_works = ArtWork.all
@@ -6,5 +7,21 @@ class ArtWorksController < ApplicationController
 
   def show
     @art_work = ArtWork.find(params[:id])
+  end
+
+  def new
+    @art_work = ArtWork.new(art_work_params)
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_art_work
+    @art_work = current_user.art_works.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def art_work_params
+    params.require(:art_work).permit(:name, :description)
   end
 end
